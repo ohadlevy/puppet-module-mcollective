ac63ff15529d73aa01843787b1a4587d1645af23
Ohad Levy <ohad.levy@infineon.com>
2010-06-09 13:15:56 +0800

    my mods

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,11 @@
+class mcollective::common {
 
-import "client.pp"
-import "service.pp"
+  $mcollective_psk            = create_and_print_password("mcollective-psk")
+  $mcollective_stomp_user     = "mcollective"
+  $mcollective_stomp_password = create_and_print_password("activemq-general-mcollective-password")
+  $mcollective_stomp_host     = "sinla011.sin.infineon.com"
+  $mcollective_stomp_port     = "61613"
+  $mcollective_factsource     = "facter"
 
+  include mcollective::service
+}
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,117 +1,106 @@
-
 class mcollective::service {
-
-    tag("autoapply")
-
-    case $operatingsystem {
-        ubuntu,debian,redhat,centos: { include mcollective::service::actual }
-        default: { notice("${hostname}: mcollective: module does not yet support $operatingsystem") }
-    }
+  case $operatingsystem {
+    ubuntu,debian,redhat,centos: { include mcollective::service::actual }
+    default: { notice("${hostname}: mcollective: module does not yet support $operatingsystem") }
+  }
 }
 
 class mcollective::service::actual {
 
-    include mcollective::install
-    include mcollective::config
-    include mcollective::plugins
+  include mcollective::install
+  include mcollective::config
+  include mcollective::plugins
 
-    service { "mcollective":
-        ensure => running,
-        hasstatus => true,
-        hasrestart => true,
-        require => Class["mcollective::install"],
-    }
-
+  service { "mcollective":
+    ensure => running,
+    hasstatus => true,
+    hasrestart => true,
+    require => Class["mcollective::install"],
+  }
 }
 
 class mcollective::config {
 
-    case $operatingsystem {
-        debian,ubuntu: { $libdir = "/usr/share/mcollective/plugins" }
-        redhat,centos: { $libdir = "/usr/libexec/mcollective" }
-    }
+  case $operatingsystem {
+    debian,ubuntu: { $libdir = "/usr/share/mcollective/plugins" }
+    redhat,centos: { $libdir = "/usr/libexec/mcollective" }
+  }
 
-    File { 
-        owner => root,
-        group => root,
-        mode  => 0440,
-        require => Class["mcollective::install"]
-    }
+  File { 
+    owner => root,
+    group => root,
+    mode  => 0440,
+    require => Class["mcollective::install"]
+  }
 
-    Dir {
-        owner => root,
-        group => root,
-        mode  => 0750,
-        require => Class["mcollective::install"]
-    }
-    
-    dir { "/etc/mcollective": }
-    file { "/etc/mcollective/server.cfg": 
-        content => template("mcollective/server.cfg.erb"),
-        notify  => Service["mcollective"]
-    }
-    file { "/etc/mcollective/facts.yaml": 
-        content => template("mcollective/facts.yaml.erb") 
-    }
-    
+  file { "/etc/mcollective":
+    ensure => directory,
+    owner => root, group => root, mode  => 0750,
+    require => Class["mcollective::install"]
+  }
+  file { "/etc/mcollective/server.cfg": 
+    content => template("mcollective/server.cfg.erb"),
+    notify  => Service["mcollective"]
+  }
+  file { "/etc/mcollective/facts.yaml": 
+    content => template("mcollective/facts.yaml.erb") 
+  }
+
 }
 
 class mcollective::plugins {
 
-    case $operatingsystem {
-        debian,ubuntu: { $p_base = "/usr/share/mcollective/plugins/mcollective" }
-        redhat,centos: { $p_base = "/usr/libexec/mcollective/mcollective" }
-    }
-    $s_base = "puppet://$fileserver/mcollective/plugins"
+  case $operatingsystem {
+    debian,ubuntu: { $p_base = "/usr/share/mcollective/plugins/mcollective" }
+    redhat,centos: { $p_base = "/usr/libexec/mcollective/mcollective" }
+  }
+  $s_base = "puppet:///mcollective/plugins"
 
-    File { 
-        owner => root,
-        group => root,
-        mode  => 0444,
-        require => Class["mcollective::install"],
-        notify => Service["mcollective"],
-    }
+  File { 
+    owner => root, group => root, mode  => 0444,
+    require => Class["mcollective::install"],
+    notify => Service["mcollective"],
+  }
 
-    file { "${p_base}/facts/facter.rb": source => "${s_base}/facts/facter/facter.rb" }
-    file { "${p_base}/agent/puppet-service.rb": source => "${s_base}/agent/service/puppet-service.rb" }
-    file { "${p_base}/agent/puppet-package.rb": source => "${s_base}/agent/package/puppet-package.rb" }
-    file { "${p_base}/agent/nrpe.rb": source => "${s_base}/agent/nrpe/nrpe.rb" }
-    file { "${p_base}/agent/puppetd.rb": source => "${s_base}/agent/puppetd/puppetd.rb" }
+  file { "${p_base}/facts/facter.rb": source => "${s_base}/facts/facter/facter.rb" }
+  file { "${p_base}/agent/puppet-service.rb": source => "${s_base}/agent/service/puppet-service.rb" }
+  file { "${p_base}/agent/puppet-package.rb": source => "${s_base}/agent/package/puppet-package.rb" }
+  file { "${p_base}/agent/nrpe.rb": source => "${s_base}/agent/nrpe/nrpe.rb" }
+  file { "${p_base}/agent/puppetd.rb": source => "${s_base}/agent/puppetd/puppetd.rb" }
 
 }
 
 class mcollective::install {
-    case $operatingsystem {
-        ubuntu,debian: { include mcollective::install::debian }
-        redhat,centos: { include mcollective::install::redhat }
-        default: { fail("${hostname}: mcollective: trying to install unsupported operatingsystem $operatingsystem") }
-    }
+  case $operatingsystem {
+    ubuntu,debian: { include mcollective::install::debian }
+    redhat,centos: { include mcollective::install::redhat }
+    default: { fail("${hostname}: mcollective: trying to install unsupported operatingsystem $operatingsystem") }
+  }
 }
 
 class mcollective::install::redhat {
 
-    package { "stomp": 
-        provider => gem, 
-        ensure => "1.1",
-    }
+  package { "stomp": 
+    name => "rubygem-stomp"
+    ensure => "installed"
+  }
 
-    package { "mcollective": 
-        ensure => present,
-        require => Package["stomp"],
-    }
+  package { "mcollective": 
+    ensure => present,
+    require => Package["stomp"],
+  }
 
 }
 
 class mcollective::install::debian {
 
-    package { "stomp": 
-        provider => gem, 
-        ensure => "1.1",
-    }
+  package { "stomp": 
+    provider => gem, 
+    ensure => "1.1",
+  }
 
-    package { "mcollective": 
-        ensure => present,
-        require => Package["stomp"],
-    }
-
+  package { "mcollective": 
+    ensure => present,
+    require => Package["stomp"],
+  }
 }
